c3d5efaa7b46fadd8ff1901603b8eb3c09a02e31
Iago <iagocardial@tecsus.com.br>
2019-09-19 20:04:49 -0300

    Adicionando HTML/CSS

---
+++ src/main/webapp/js/efeitos.js
@@ -0,0 +1,15 @@
+$('#buscar').on('click',function(){
+    $('.table-striped').show();
+});
+
+$('#buscar').on('click',function(){
+    $('#Finalizar').show();
+});
+
+$('#registrar').on('click',function(){
+    $('.alert-success').show();
+});
+
+$('#Finalizar').on('click',function(){
+    $('.alert-success').show();
+});
